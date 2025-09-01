b1098fe6ae746864c3acb065175d453a5c1b5e18
Miguel Sarmiento <125526011+SeMeCalma@users.noreply.github.com>
2025-08-31 19:24:49 -0500

    Delete src/main/java/edu/universidad directory

--- src/main/java/edu/universidad/repository/Servicios.java
+++
@@ -1,9 +0,0 @@
-package edu.universidad.repository;
-
-import java.util.List;
-
-public interface Servicios<T> {
-    String imprimirPosicion(int posicion);
-    Integer cantidadActual();
-    List<String> imprimirListado();
-}
